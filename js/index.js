d5d5f512e468fc35b7f649fea33d8b2bdafeb41e
ya-galyuk <ya.galyuk@gmail.com>
2021-04-30 20:53:47 +0300

    add menu

--- js/index.js
+++ js/index.js
@@ -70,31 +70,31 @@ let pageSlider = new Swiper('.page', {
     }
 });
 
-let menuLinks = document.querySelectorAll('.menu__link');
-
-function menuSlider() {
-    if (menuLinks.length > 0) {
-        menuLinks[pageSlider.realIndex].classList.add('_active')
-
-        for (let i = 0; i < menuLinks.length; i++) {
-            const menuLink = menuLinks[i];
-
-            menuLink.addEventListener('click', function (e) {
-                menuSliderRemove();
-                pageSlider.slideTo(i, 800);
-                menuLink.classList.add('_active')
-                e.preventDefault();
-            });
-        }
-    }
-}
-
-function menuSliderRemove() {
-    let menuLinkActive = document.querySelector('.menu__link._active');
-    if (menuLinkActive) {
-        menuLinkActive.classList.remove('_active');
-    }
-}
+// let menuLinks = document.querySelectorAll('.menu__link');
+//
+// function menuSlider() {
+//     if (menuLinks.length > 0) {
+//         menuLinks[pageSlider.realIndex].classList.add('_active')
+//
+//         for (let i = 0; i < menuLinks.length; i++) {
+//             const menuLink = menuLinks[i];
+//
+//             menuLink.addEventListener('click', function (e) {
+//                 menuSliderRemove();
+//                 pageSlider.slideTo(i, 800);
+//                 menuLink.classList.add('_active')
+//                 e.preventDefault();
+//             });
+//         }
+//     }
+// }
+//
+// function menuSliderRemove() {
+//     let menuLinkActive = document.querySelector('.menu__link._active');
+//     if (menuLinkActive) {
+//         menuLinkActive.classList.remove('_active');
+//     }
+// }
 
 function setScrollType() {
     if (wrapper.classList.contains('_free')) {
@@ -413,28 +413,44 @@ function scrollFunction() {
 
 // ----------------------------------------------*/
 
-//
-// const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-// if (menuLinks.length > 0){
-//     menuLinks.forEach(menuLink => {
-//         menuLink.addEventListener('click', onMenuLinkClick);
-//     });
-//
-//     function onMenuLinkClick(e){
-//         const menuLink = e.target;
-//         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-//             const gotoBlock = document.querySelector(menuLink.dataset.goto);
-//             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-//
-//             window.scrollTo({
-//                 top:gotoBlockValue,
-//                 behavior: "smooth"
-//             });
-//
-//             e.preventDefault();
-//         }
-//     }
-// }
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu){
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function (e){
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if(iconMenu.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top:gotoBlockValue,
+                behavior: "smooth"
+            });
+
+            e.preventDefault();
+        }
+    }
+}
 
 
 
